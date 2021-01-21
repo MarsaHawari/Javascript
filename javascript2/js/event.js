16dd72f491676549ac6fc95c0f0b5ff71d5c1897
MarsaHawari <68333057+MarsaHawari@users.noreply.github.com>
2021-01-21 23:55:53 +0700

    Add files via upload

---
+++ javascript2/js/event.js
@@ -0,0 +1,12 @@
+function tampil(b) {
+
+a = document.querySelector("p").innerText="belajar text event "+ b;
+//a.innerText = "belajar event js";
+
+    console.log("belajar event js");
+}
+
+judul.onclick= function () {
+   
+    document.querySelector(".isi").innerHTML = "belajar html menggunakan id";
+};
